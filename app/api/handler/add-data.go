7946d071840ad0ee7fcd4399159d790ebf0ea6ce
abinash <abinashphulkonwar1@gmail.com>
2023-08-17 21:36:43 +0530

    add set data list

--- app/api/handler/add-data.go
+++ app/api/handler/add-data.go
@@ -1,8 +1,40 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/abinashphulkonwar/redis/storage"
+	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	LIST   = "LSET"
+	TEXT   = "SET"
+	NUMBER = "SET"
+)
 
 func AddData(c *fiber.Ctx) error {
+	buf := c.Body()
+
+	body := storage.Data{}
+
+	err := json.Unmarshal(buf, &body)
+
+	if err != nil {
+		return err
+	}
+
+	if body.Type == LIST {
+		return ListHandler(c, &body)
+	}
+
+	return c.JSON(fiber.Map{
+		"status": "ok",
+	})
+}
+
+func ListHandler(c *fiber.Ctx, body *storage.Data) error {
+
 	return c.JSON(fiber.Map{
 		"status": "ok",
 	})
