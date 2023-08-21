aea6cd7ef2299aa9c33cb8b656a5c8be828b15a2
abinash <abinashphulkonwar1@gmail.com>
2023-08-21 20:34:44 +0530

    add lpush, rpush, if not exist

--- app/storage/processor-list.go
+++ app/storage/processor-list.go
@@ -4,26 +4,42 @@ import (
 	"github.com/abinashphulkonwar/redis/commands"
 )
 
+func AddValToList(data *DBCommands, list *List) {
+	if data.Payload.Commands == commands.C_LPUSH {
+		println("list LPUSH")
+		list.LPush(data.Payload.Data.Value)
+
+	} else {
+		println("list RPUSH")
+		list.RPush(data.Payload.Data.Value)
+	}
+}
+
 func ListProcessor(data *DBCommands) {
 
-	if data.Payload.Type == commands.LSET {
-		list := Init()
-		data.Payload.Data.Value = list
-	} else if data.Payload.Type == commands.C_LPUSH || data.Payload.Type == commands.C_RPUSH {
-		listRef, isFound := Get(data.Payload.Key)
-		if isFound {
-			if listRef.Type == commands.LSET {
-				list := listRef.Value.(*List)
-				if data.Payload.Type == commands.C_LPUSH {
-					list.LPush(data.Payload.Data.Value)
-				} else {
-					list.RPush(data.Payload.Data.Value)
-				}
-			} else {
-				list := Init()
-				data.Payload.Data.Value = list
-			}
-		}
+	listRef, isFound := Get(data.Payload.Key)
+	var list *List = nil
+
+	if data.Payload.IfNotExist && listRef != nil {
+		println("data.Payload.IfNotExist")
+		data.Payload.Data.Value = listRef.Value
+		data.Payload.Data.Type = listRef.Type
+		data.Payload.Data.EX = listRef.EX
+		return
 	}
+
+	if isFound {
+		if listRef.Type == commands.LSET {
+			list = listRef.Value.(*List)
+		} else {
+			list = Init()
+		}
+	} else {
+		list = Init()
+
+	}
+	AddValToList(data, list)
+	data.Payload.Data.Value = list
+	data.Payload.Data.Type = commands.LSET
 
 }
