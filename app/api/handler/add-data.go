47db99a6fe4752c6930b3c175f7720c060735fb4
abinash <abinashphulkonwar1@gmail.com>
2023-09-04 20:49:35 +0530

    add number float64 to int conversion

--- app/api/handler/add-data.go
+++ app/api/handler/add-data.go
@@ -20,26 +20,46 @@ func AddData(queue *storage.Queue) func(c *fiber.Ctx) error {
 		if err != nil {
 			return err
 		}
+
 		switch body.Type {
 		case commands.LSET:
-			InsertToQueue(c, &body, queue)
+			return InsertToQueue(c, &body, queue)
 		case commands.TEXT:
-			InsertToQueue(c, &body, queue)
+			return InsertToQueue(c, &body, queue)
+		case commands.NUMBER:
+			intVal, isInt := body.Data.Value.(int)
+			floatVal, isFloat := body.Data.Value.(float64)
+			if !isInt && !isFloat {
+				return fiber.NewError(fiber.StatusUnprocessableEntity, "value of 'type' 'number' key should be int")
+			}
+			if isInt {
+				body.Data.Value = intVal
+			}
+			if isFloat {
+				body.Data.Value = int(floatVal)
+
+			}
+			return InsertToQueue(c, &body, queue)
 		}
 
-		return c.JSON(fiber.Map{
-			"status": "ok",
-			"body":   body.Data,
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"status":  "error",
+			"body":    body.Data,
+			"message": "unvalid command",
 		})
 	}
 	return handler
 }
 
-func InsertToQueue(c *fiber.Ctx, body *storage.RequestBody, queue *storage.Queue) {
+func InsertToQueue(c *fiber.Ctx, body *storage.RequestBody, queue *storage.Queue) error {
 
 	queue.Insert(&storage.DBCommands{
 		Connection: c,
 		Payload:    body,
+	})
+	return c.JSON(fiber.Map{
+		"status": "error",
+		"body":   body.Data,
 	})
 
 }
