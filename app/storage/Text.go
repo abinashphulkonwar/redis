742a619a4b73f102b5d7647e7e5da9aa60f87fee
abinash <abinashphulkonwar1@gmail.com>
2023-10-19 18:29:00 +0530

    add text processor

--- app/storage/Text.go
+++ app/storage/Text.go
@@ -1,11 +1,14 @@
 package storage
 
+import "github.com/abinashphulkonwar/redis/commands"
+
 func TextProcessor(data *DBCommands) {
 
 	_, isFound := Get(data.Payload.Key)
-	if (!isFound && data.Payload.IfNotExist) || (isFound && !data.Payload.IfNotExist) || (!isFound && !data.Payload.IfNotExist) {
-		data.Payload.Data.Type = data.Payload.Type
-		Set(data.Payload.Key, &data.Payload.Data)
+	if isFound && data.Payload.IfNotExist {
+		return
 	}
+	data.Payload.Data.Type = commands.TYPE_STRING
+	Set(data.Payload.Key, &data.Payload.Data)
 
 }
