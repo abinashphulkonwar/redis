6177c6c53989b9f53b0f8b63df052eb8c7065793
abinash <abinashphulkonwar1@gmail.com>
2023-10-19 18:27:21 +0530

    add number inc

--- app/storage/Number.go
+++ app/storage/Number.go
@@ -1,14 +1,23 @@
 package storage
 
+import "github.com/abinashphulkonwar/redis/commands"
+
 func NumberProcessor(data *DBCommands) {
 	val, isFound := Get(data.Payload.Key)
-	data.Payload.Data.Type = data.Payload.Type
-	if (!isFound && data.Payload.IfNotExist) || (isFound && !data.Payload.IfNotExist) {
-		Set(data.Payload.Key, &data.Payload.Data)
-	} else {
+	data.Payload.Data.Type = commands.TYPE_NUMBER
+
+	if isFound && data.Payload.IfNotExist {
+		return
+	} else if isFound && val.Type == commands.TYPE_NUMBER {
+		println("data inc")
 		data.Payload.Data.Value = val.Value.(int) + data.Payload.Data.Value.(int)
 		data.Payload.Data.Type = val.Type
 		data.Payload.Data.EX = val.EX
 		Set(data.Payload.Key, &data.Payload.Data)
+	} else {
+		Set(data.Payload.Key, &data.Payload.Data)
+		println("data add defult")
+
 	}
+
 }
