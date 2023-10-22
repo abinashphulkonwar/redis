bfca2a2b74a7bb42dca51528e6128e9b7c4bf018
abinash <abinashphulkonwar1@gmail.com>
2023-10-22 17:04:44 +0530

    add api logger

--- app/api/api.go
+++ app/api/api.go
@@ -4,12 +4,15 @@ import (
 	"github.com/abinashphulkonwar/redis/api/routes"
 	"github.com/abinashphulkonwar/redis/storage"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func App(queue *storage.Queue) *fiber.App {
 	app := fiber.New()
 
+	app.Use(logger.New())
 	app.Route("/api/write", routes.InsertHandler(queue))
 	app.Route("/api/query", routes.QueryHandler(queue))
+
 	return app
 }
