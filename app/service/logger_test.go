87a36c5287c6da2ea10590d2abfb66a8e245a7f4
abinash <abinashphulkonwar1@gmail.com>
2023-10-23 20:01:56 +0530

    add test for read

--- app/service/logger_test.go
+++ app/service/logger_test.go
@@ -7,17 +7,22 @@ import (
 	"github.com/abinashphulkonwar/redis/service"
 )
 
-func TestLogger(t *testing.T) {
+func TestLoggerWrite(t *testing.T) {
 	os.Setenv("mode", "Test")
-	logger := service.InitLogger("log.txt")
+	logger := service.InitLogger("log")
 	logger.Add(&service.Log{
 		Time:    "sjkjsd",
 		Status:  "1",
 		Path:    "string",
-		Method:  "string\njabsjh",
+		Method:  "stringjabsjh",
 		Command: "string",
 		Key:     "string",
-		Value:   "string",
+		Value:   "string\najbjabd",
 	})
 	logger.New()
+}
+func TestLoggerRead(t *testing.T) {
+	os.Setenv("mode", "Test")
+	logger := service.InitLogger("log")
+	logger.Read()
 }
