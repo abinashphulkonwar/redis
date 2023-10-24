e7ffc1bf04606d02fb8e397354d1899b918d5b93
abinash <abinashphulkonwar1@gmail.com>
2023-10-24 11:51:10 +0530

    add fake data

--- app/service/logger_test.go
+++ app/service/logger_test.go
@@ -2,22 +2,25 @@ package service_test
 
 import (
 	"os"
+	"strconv"
 	"testing"
 
 	"github.com/abinashphulkonwar/redis/service"
+	"github.com/brianvoe/gofakeit/v6"
 )
 
 func TestLoggerWrite(t *testing.T) {
 	os.Setenv("mode", "Test")
 	logger := service.InitLogger("log")
+
 	logger.Add(&service.Log{
-		Time:    "sjkjsd",
-		Status:  "1",
-		Path:    "string",
-		Method:  "stringjabsjh",
-		Command: "string",
-		Key:     "string",
-		Value:   "string\najbjabd",
+		Time:    gofakeit.Date().String(),
+		Status:  strconv.Itoa(gofakeit.HTTPStatusCode()),
+		Path:    gofakeit.URL(),
+		Method:  gofakeit.HTTPMethod(),
+		Command: gofakeit.Username(),
+		Key:     gofakeit.RandomString([]string{gofakeit.Name(), gofakeit.Email()}),
+		Value:   gofakeit.RandomString([]string{gofakeit.Name(), gofakeit.Email(), gofakeit.Username()}),
 	})
 	logger.New()
 }
