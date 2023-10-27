23ed54e87a269d51cd4dfa2e48abda69df54f927
abinash <abinashphulkonwar1@gmail.com>
2023-10-27 14:37:47 +0530

    add write func

--- client/src/write.ts
+++ client/src/write.ts
@@ -2,29 +2,32 @@ import fetch from "node-fetch";
 import { GetConfig } from "./init";
 import { ErrorInterface, RedisClientError } from "./error";
 
-export const Get = async ({
-  Key,
-  Command,
-}: {
+export const Write = async (event: {
+  Data: {
+    value: string;
+    EX?: number;
+  };
   Key: string;
-  Command: string;
+  Commands: string;
+  IfNotExist: boolean;
 }) => {
   const config = GetConfig();
-  const request = await fetch(
-    `${config.url}/api/query/GET?command=${Command} ${Key}`,
-    {
-      method: "GET",
-      headers: {
-        "content-type": "application/json",
-      },
-    }
-  );
+  const request = await fetch(`${config.url}/api/write/add`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(event),
+  });
 
   if (request.ok) {
     const res = (await request.json()) as unknown as {
+      body: {
+        Value: string;
+        EX: number;
+        Type: string;
+      };
       status: string;
-      data: string;
-      Is_LIST?: boolean;
     };
     return res;
   }
