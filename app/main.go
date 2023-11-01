fd41b92ce3cc4a3d18fc0baa53684db1cca03509
abinash <abinashphulkonwar1@gmail.com>
2023-11-01 18:24:43 +0530

    add logger handler

--- app/main.go
+++ app/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"github.com/abinashphulkonwar/redis/api"
+	"github.com/abinashphulkonwar/redis/internalstorage"
+	"github.com/abinashphulkonwar/redis/service"
 	"github.com/abinashphulkonwar/redis/storage"
 )
 
 func main() {
-	queue := storage.InitQueue()
+	queue := internalstorage.InitQueue()
 	app := api.App(queue)
-	go storage.DBCommandsHandler(queue)
+	loger := service.InitLogger("log")
+	go loger.New()
+	go storage.DBCommandsHandler(queue, loger)
 	app.Listen(":3000")
 }
